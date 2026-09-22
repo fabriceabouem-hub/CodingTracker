12b324ca9347d55edfc3d905b68028fe9c8e5206
Fabrice Dylan <fabrice.contact@gamail.com>
2026-09-22 13:39:28 +0200

    Ghost

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! Comment allez vous ?");
+            Console.WriteLine("Hello, Welt!");
+            Ghost ghost = new Ghost("Spooky");
+            ghost.Haunt();
+            SlimeGhost slimeghost = new SlimeGhost("Slimey");
+            Console.WriteLine($"Der Name von Slime ist : {slimeghost.Name}");
+            slimeghost.Haunt();
+        }
+    }
+    class Ghost
+    {
+        public string Name { get; set; }
+
+        public Ghost(string name)
+        {
+            Name = name;
+        }   
+        public virtual void Haunt()
+        {
+            Console.WriteLine($"Der Ghost {Name} sagt : Buh ");
+        }
+
+    }
+    class SlimeGhost : Ghost
+    {
+        public SlimeGhost(string name) : base(name)
+        {
+
+        }
+        public override void Haunt()
+        {
+            this.Slime();
+            base.Haunt();
+        }
+        public void Slime()
+        {
+            Console.WriteLine($"Der SlimeGhost {Name} hinterlässt eine Schleimspur!");
         }
     }
 }
